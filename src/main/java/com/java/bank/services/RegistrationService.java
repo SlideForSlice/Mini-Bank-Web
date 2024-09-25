a4edef79c87e9d1f67b933911eff7983dd8ba588
konstantinus1337 <166991398+konstantinus1337@users.noreply.github.com>
2024-09-25 20:21:34 +0500

    added security

---
+++ src/main/java/com/java/bank/services/RegistrationService.java
@@ -0,0 +1,27 @@
+package com.java.bank.services;
+
+import com.java.bank.models.User;
+import com.java.bank.repositories.BankAccountRepository;
+import com.java.bank.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RegistrationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RegistrationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public void register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+}
---
+++ src/main/java/com/java/bank/services/UserDetailService.java
@@ -0,0 +1,33 @@
+package com.java.bank.services;
+
+import com.java.bank.models.User;
+import com.java.bank.repositories.UserRepository;
+import com.java.bank.security.UserDetail;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserDetail(user.get());
+    }
+}
