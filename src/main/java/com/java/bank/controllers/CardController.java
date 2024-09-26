7802a160ad6e23168590e32264bade8896113b68
Lil master <kenigilya9@gmail.com>
2024-09-27 01:21:26 +0500

    Added smth

--- src/main/java/com/java/bank/controllers/CardController.java
+++ src/main/java/com/java/bank/controllers/CardController.java
@@ -1,10 +1,10 @@
 package com.java.bank.controllers;
 
-import com.java.bank.DTO.BankAccountDTO;
 import com.java.bank.models.BankAccount;
 import com.java.bank.models.Card;
 import com.java.bank.services.CardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,16 +16,35 @@ import java.util.Optional;
 public class CardController {
     private final CardService cardService;
 
-    @GetMapping
+    @GetMapping()
     public Optional<Card> getAllCards(BankAccount idBankAccount) {
         return cardService.getAllCardsByBankAccount(idBankAccount);
         //TODO Добавить ссылку на страницу
     }
 
-    @PostMapping("{idBankAccount}")
+    @PostMapping("/create-card/{idBankAccount}")
     public void createCard(@PathVariable BankAccount idBankAccount) {
         cardService.createCard(idBankAccount);
     }
 
+    @DeleteMapping("/delete-card/{cardId}")
+    public void deleteCurrentCard(@PathVariable int cardId) {
+        cardService.deleteCard(cardId);
+    }
+
+    @PutMapping("/cash-in/{cardId}")
+    public void cashIn(@PathVariable int cardId, float amount) {
+        cardService.cashIn(cardId, amount);
+    }
+
+    @PutMapping("/cash-out/{cardId}")
+    public void cashOut(@PathVariable int cardId, float amount) {
+        cardService.cashOut(cardId, amount);
+    }
+
+    @PutMapping("/send/{cardId}")
+    public void send(@PathVariable int cardId, float amount, String recieverCardNum) {
+        cardService.sendMoneyOnOtherCard(cardId, amount, recieverCardNum);
+    }
 
 }
--- src/main/java/com/java/bank/repositories/CreditRepository.java
+++ src/main/java/com/java/bank/repositories/CreditRepository.java
@@ -1,11 +1,14 @@
 package com.java.bank.repositories;
 
 import com.java.bank.models.BankAccount;
+import com.java.bank.models.Card;
 import com.java.bank.models.Credit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CreditRepository extends JpaRepository<Credit, Integer> {
     List<Credit> findByBankAccount(BankAccount bankAccount);
+
 }
