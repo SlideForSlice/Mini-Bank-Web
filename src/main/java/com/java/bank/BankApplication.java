8b98d254c80afab32c3226f1901c5665e3b64457
konstantinus1337 <166991398+konstantinus1337@users.noreply.github.com>
2024-10-06 12:39:58 +0500

    added smth

--- src/main/java/com/java/bank/BankApplication.java
+++ src/main/java/com/java/bank/BankApplication.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class BankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankApplication.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(BankApplication.class, args);
+//	}
 
 }
